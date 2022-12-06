373f42c47138f55caf52941202f409f3c7fdbffc
Vicky Banks <victoria.banks052@gmail.com>
2022-12-06 20:59:56 +0000

    cleaned

--- day6_signal_device.R
+++ day6_signal_device.R
@@ -6,9 +6,9 @@ signal
 
 ### task1 find the first time 4 characters that have no repeats are seen
 ## task 2 find the first time 14 characters that have no repeats are seen
-signal_length<-substring(signal, 1:nchar(signal), 1:nchar(signal)) %>% length()
+nchar(signal)
 
-for(x in 1:signal_length) {
+for(x in 1:nchar(signal)) {
   
   sub <- substr(signal, x, x + 13)## chunk into 4 characters
   print(sub)
